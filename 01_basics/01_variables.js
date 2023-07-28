b9a5882635078031187d276534e9241f48f5925d
Vivevk <140709184+Vivevk@users.noreply.github.com>
2023-07-28 07:56:08 +0000

    dataType

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 144553
+let accountEmail = "juhiyede@gooogale.com"
+var accountPassword = "123"
+accountCity = "jaipur"
+let accountState;
+//accountId =2 //not  allowed
+accountEmail = "hc@hc.cpm"
+accountPassword = "12345"
+accountCity = "Raipur"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity,accountState])
+
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //trat al js code newer virsion
+//alert("JUHI HELLO")//we are using node.js ,not browser
+
+let name = "juhi"
+let age = 18
+let isLoggesIN = false
+
+//number=> 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null =>stand alone value 
+//undefind =>
+//symbol =>unique 
+
+//object 
+
+console.log(typeof undefined)//undefind
+console.log(typeof null)//object
